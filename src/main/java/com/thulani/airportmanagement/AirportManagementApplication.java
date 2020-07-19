5fe563ff539d7cde0cfed39713845b6b3c58e36a
thue101 <56626050+thue101@users.noreply.github.com>
2020-07-20 01:16:26 +0200

    first commit

---
+++ src/main/java/com/thulani/airportmanagement/AirportManagementApplication.java
@@ -0,0 +1,13 @@
+package com.thulani.airportmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportManagementApplication.class, args);
+	}
+
+}
